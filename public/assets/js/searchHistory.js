707935df6e67119d5227580a890bba55e610fa77
BansiKalariya <kalariyabansi5577@gmail.com>
2024-11-30 16:29:48 -0500

    Update searchHistory.js with new functionality

--- public/assets/js/searchHistory.js
+++ public/assets/js/searchHistory.js
@@ -1,7 +1,7 @@
 function loadSearchHistory() {
     const searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
     const searchHistoryContainer = document.getElementById('searchHistory');
-
+// to commit changes
     searchHistory.forEach(term => {
         const termElement = document.createElement('a');
         termElement.textContent = term;
